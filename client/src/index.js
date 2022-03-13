7460912578fdaa3e5104a651f6bff3e0f2a5a016
Alex <alexgrabko1992@gmail.com>
2022-03-13 22:14:13 +0300

    fix 11

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React, { createContext } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
-import ReviewStore from "./store/ReviewStore";
 import UserStore from "./store/UserStore";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
--- client/src/store/ReviewStore.js
+++
@@ -1,5 +0,0 @@
-import { makeAutoObservable } from "mobx";
-
-export default class ReviewStore {
-  constructor() {}
-}
